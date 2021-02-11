2f2679b715ab7942c7b762eb0a47ad232b6d4f6a
Sahil Mishra <sahilmishra0012@outlook.com>
2021-02-11 07:44:15 +0530

    modified fetchData function to get patient details

--- src/app/contexts/FirestoreContext.js
+++ src/app/contexts/FirestoreContext.js
@@ -4,11 +4,25 @@ import { db } from "../components/firebase"
 
 
 export const fetchPatientData = async (value) => {
-    const docRef = db.collection('patientIDUID').doc(value.trim());
-    docRef.get().then((doc) => {
+    const uidRef = db.collection('patientIDUID').doc(value.trim());
+    uidRef.get().then((doc) => {
         if (doc.exists) {
             let data = doc.data();
-            console.log("Document data:", data);
+            const patientRef = db.collection('patients').doc(data['uid'])
+            patientRef.get().then((doc) => {
+                if (doc.exists) {
+                    let data = doc.data();
+                    console.log(data['name']);
+                    console.log(data['dob'])
+                }
+                else {
+                    console.log("No such document!");
+                }
+
+            }).catch(function (error) {
+                console.log("Error getting document:", error);
+            });
+
         } else {
             console.log("No such document!");
         }
