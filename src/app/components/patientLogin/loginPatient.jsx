05ee94330d2769d206ace4fb7372d7c57d7aed6b
Sahil Mishra <sahilmishra0012@outlook.com>
2021-02-12 08:37:27 +0530

    implemented Firestore

--- src/app/components/patientLogin/loginPatient.jsx
+++ src/app/components/patientLogin/loginPatient.jsx
@@ -10,6 +10,7 @@ import Alert from '@material-ui/lab/Alert';
 import logo from "../../../images/logo.png"
 import containerImage from "../../../images/7882.png"
 import { useAuth } from "../../contexts/AuthContext";
+import { fetchPatientData } from "../../contexts/FirestoreContext";
 import { useHistory } from "react-router-dom";
 
 export default function LoginPatient() {
@@ -25,7 +26,7 @@ export default function LoginPatient() {
         try {
             setError("")
             setLoading(true)
-            await login(uidRef.current.value)
+            await fetchPatientData(uidRef.current.value)
             history.push("/patientProfile")
         } catch {
             setError("Failed to log in")
---
+++ src/app/contexts/FirestoreContext.js
@@ -0,0 +1,52 @@
+import React, { useContext, useState, useEffect } from "react"
+import { db } from "../components/firebase"
+
+export const fetchDoctorName = async (value) => {
+    const uidRef = db.collection('doctors').doc('5o441GVIMqb1CLbREqL2DKLTseG2');
+    uidRef.get().then((doc) => {
+        if (doc.exists) {
+            let data = doc.data();
+            return data;
+        }
+        else {
+            console.log("No such document!");
+            return -999;
+        }
+    }).catch(function (error) {
+        console.log("Error getting document:", error);
+        return -999;
+    });
+}
+
+
+export const fetchPatientData = async (value) => {
+    const uidRef = db.collection('patientIDUID').doc(value.trim());
+    uidRef.get().then((doc) => {
+        if (doc.exists) {
+            let data = doc.data();
+            const patientRef = db.collection('patients').doc(data['uid'])
+            patientRef.get().then((doc) => {
+                if (doc.exists) {
+                    let data = doc.data();
+                    console.log(data);
+                    return data;
+                }
+                else {
+                    console.log("No such document!");
+                    return -999;
+                }
+
+            }).catch(function (error) {
+                console.log("Error getting document:", error);
+                return -999;
+            });
+
+        } else {
+            console.log("No such document!");
+            return -999;
+        }
+    }).catch(function (error) {
+        console.log("Error getting document:", error);
+        return -999;
+    });
+}
