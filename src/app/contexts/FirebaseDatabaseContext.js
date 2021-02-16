b7293ededb55a66c436c44193b63616a4e7fd0cc
Sahil Mishra <sahilmishra0012@outlook.com>
2021-02-16 08:58:37 +0530

    Removed random otp generator and added it to loginPatient.jsx

--- src/app/contexts/FirebaseDatabaseContext.js
+++ src/app/contexts/FirebaseDatabaseContext.js
@@ -1,7 +1,6 @@
 import { rdbms, db } from "../components/firebase"
 
-export const generateOTP = async (value) => {
-    const rand = Math.floor(100000 + Math.random() * 900000);
+export const generateOTP = async (value, rand) => {
     const pidRef = db.collection('patientIDUID').doc(value.trim());
     const uidDoc = await pidRef.get()
     if (uidDoc.exists) {
@@ -9,6 +8,21 @@ export const generateOTP = async (value) => {
         rdbms.ref('otp/' + data).set({
             otp: rand,
         });
+    }
+    else {
+        console.log("No such patient!");
+        return -999;
+    }
+}
+
+export const getOTP = async (value) => {
+    const pidRef = db.collection('patientIDUID').doc(value.trim());
+    const uidDoc = await pidRef.get()
+    if (uidDoc.exists) {
+        let data = uidDoc.data()['uid'];
+        rdbms.ref('otp/' + data).on("value", (snapshot) => {
+            return snapshot.val();
+        })
     }
     else {
         console.log("No such patient!");
